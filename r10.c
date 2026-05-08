0143b05bb82e41c9daaf22fac170760d7de87797
ray305280-code <ray305280@gmail.com>
2026-05-08 16:53:07 +0530

    Add files via upload

---
+++ r10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+// find out maximum number of given numbers
+int main()
+{
+	int a = 199, b = 55;
+	
+	if(a > b){
+		printf("B is Max");
+	}
+}
---
+++ r9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// find out given number is positive or not
+int main()
+{
+	int num = 10;
+	
+	if(num < 0){
+		printf("Negative Number");
+	}else{
+		printf("Positive Number");
+	}
+}
